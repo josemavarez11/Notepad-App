21c6e7643519588c70ede3c83a0be7c03d2b4c87
Misa24jr <misa24jr@gmail.com>
2024-02-17 00:33:17 -0400

    Resuleto el error temporal

--- screens/components/Tabs/Note/Note.jsx
+++ screens/components/Tabs/Note/Note.jsx
@@ -93,7 +93,6 @@ const Note = () => {
     return (
         <FlatList
             data={info}
-            keyExtractor={(item) => item.id.toString()} // Add keyExtractor prop to provide a unique key for each item
             id={info.id}
             renderItem={({ item: info}) => (
                
@@ -359,7 +358,7 @@ const Note = () => {
                     </TouchableOpacity>
                     
             )}
-            //keyExtractor={(item) => item.name}
+            keyExtractor={(item) => item.name}
             showsHorizontalScrollIndicator={false}
             showsVerticalScrollIndicator={false}r
             style={{ height: "100%" }}
